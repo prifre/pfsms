59274fd22583af7ba3f8770be4d229f37e9e7c06
prifre <prifre@prifre.com>
2024-06-28 22:12:32 +0200

    modified:   ui/table.go

--- ui/table.go
+++ ui/table.go
@@ -28,7 +28,7 @@ func (s *thetable) buildTable() *container.Scroll {
 //	var data = [][]string{{"A1", "B1"},{"A2", "B2"},{"A3", "B3"},{"A4", "B4"},{"A5", "B5"}}
 	var data = [][]string{{}}
 	for i:=0;i<100;i++ {
-		data=append(data,[]string{fmt.Sprintf("A%d",i),fmt.Sprintf("B%d",i)})
+		data=append(data,[]string{fmt.Sprint(i),fmt.Sprintf("A%d",i),fmt.Sprintf("B%d",i)})
 	}
 	list := widget.NewTable(
 	func() (int, int) {
