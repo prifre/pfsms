b2ced362ad66b4bd3a43cedd6c5ae72e7b29f7df
prifre <prifre@prifre.com>
2025-12-30 09:27:25 +0100

    x6

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+//Main file
+
+import (
+	"pfsms/ui"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+
+func main() {
+	var wx,wy float32
+	a := app.NewWithID("pfsms")
+	w := a.NewWindow("pfsms")
+	wx=1024
+	wy=768
+	w.Canvas().Content().Resize(fyne.NewSize(wx,wy))
+	w.Resize(fyne.NewSize(wx,wy))
+	w.SetContent(ui.Create(w))
+	w.ShowAndRun()
+}
